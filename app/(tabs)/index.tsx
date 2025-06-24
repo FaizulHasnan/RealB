8ecfbb185a730b2c44b5d0aac82a42bb317b6b73
Rizal <sz7251@att.com>
2025-06-24 15:16:50 +0800

    changes from rizal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,32 @@
-import PlayerList from "@/components/PlayerList.tsx";
+import PlayerList from "@/components/PlayerList";
 import { Players } from "@/constants/Players";
 import { router } from "expo-router";
+import { useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  // const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  
+  // useEffect(() => {
+  //   // Fetch data from Supabase when the component mounts
+  //   getData();
+  // }, []);
+
+  // const getData = async () => {
+  //   try{
+  //     const { data, error, status} = await supabase.from('users').select('*');
+  //     if(error && status !== 406) {
+  //       throw error;
+  //     }
+  //     if(data) {
+  //       console.log('Data fetched successfully:', data);
+  //     }
+  //   }catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Text
